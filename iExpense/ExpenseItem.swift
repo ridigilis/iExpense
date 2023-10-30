6ce9994e3316eadae94716005a0be6b925dfadc7
rdgronii <rgroner2@gmail.com>
2023-10-30 14:07:45 -0500

    build a list we can delete from

---
+++ iExpense/ExpenseItem.swift
@@ -0,0 +1,16 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Ricky David Groner II on 10/30/23.
+//
+
+import SwiftUI
+
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+
---
+++ iExpense/Expenses.swift
@@ -0,0 +1,13 @@
+//
+//  Expenses.swift
+//  iExpense
+//
+//  Created by Ricky David Groner II on 10/30/23.
+//
+
+import SwiftUI
+
+@Observable
+class Expenses {
+    var items = [ExpenseItem]()
+}
