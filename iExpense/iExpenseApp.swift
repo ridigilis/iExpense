2fa16350e65d6aaa54fd29bb22cbcab256729406
rdgronii <rgroner2@gmail.com>
2023-10-30 13:38:32 -0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Ricky David Groner II on 10/30/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
